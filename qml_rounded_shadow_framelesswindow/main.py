4333f43992af36f741d00a22e18c3233b0f7a5d2
Jung Gyu Yoon <yjg30737@gmail.com>
2022-06-02 18:28:39 +0900

    Update __init__.py, main.py to make it easy to access

--- qml_rounded_shadow_framelesswindow/main.py
+++ qml_rounded_shadow_framelesswindow/main.py
@@ -1,10 +1,12 @@
-import sys
+import sys, os
 
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
-app = QGuiApplication(sys.argv)
-engine = QQmlApplicationEngine()
-engine.quit.connect(app.quit)
-engine.load('main.qml')
-sys.exit(app.exec())
+def showQml():
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.quit.connect(app.quit)
+    p = os.path.dirname(__file__)
+    engine.load(os.path.join(p, 'main.qml'))
+    sys.exit(app.exec())
