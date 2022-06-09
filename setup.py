230f2d791f17caca3db58ef071467cf42e3a162b
Jung Gyu Yoon <yjg30737@gmail.com>
2022-06-09 16:12:12 +0900

    Update package_data

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='yjg30737@gmail.com',
     license='MIT',
     packages=find_packages(),
-    package_data={'qml_rounded_shadow_framelesswindow': ['main.qml'],
+    package_data={'qml_rounded_shadow_framelesswindow': ['main.qml', 'settings.qml'],
                   'qml_rounded_shadow_framelesswindow.images': ['peace.svg']},
     description='Rounded shadow frameless window made out of qml, execute with PyQt5',
     url='https://github.com/yjg30737/qml-rounded-shadow-framelesswindow.git',
